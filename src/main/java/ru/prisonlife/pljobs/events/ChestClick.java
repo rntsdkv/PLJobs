26bbdbab1d75e74de6a5529c845a5fcbb7bc4e68
Waldemar Sannicov <sannicov.hero@gmail.com>
2020-07-04 16:58:11 +0600

    оптимизируем событие создания мусорного бака

--- src/main/java/ru/prisonlife/pljobs/events/ChestClick.java
+++ src/main/java/ru/prisonlife/pljobs/events/ChestClick.java
@@ -3,6 +3,7 @@ package ru.prisonlife.pljobs.events;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,37 +12,38 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import ru.prisonlife.plugin.PLPlugin;
 
+import java.util.Optional;
+
 import static ru.prisonlife.pljobs.Main.colorize;
 import static ru.prisonlife.pljobs.commands.SetGarbage.garbagePlayers;
 
 public class ChestClick implements Listener {
 
-    private PLPlugin plugin;
+    private final PLPlugin plugin;
 
-    public ChestClick(PLPlugin main) {
-        this.plugin = main;
+    public ChestClick(PLPlugin plugin) {
+        this.plugin = plugin;
     }
 
     @EventHandler
     public void onChestClick(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        Action action = event.getAction();
-        if (action == Action.LEFT_CLICK_BLOCK || action == Action.RIGHT_CLICK_BLOCK) {
-            Block block = event.getClickedBlock();
-            if (block.getType() == Material.CHEST && garbagePlayers.contains(player)) {
-                String path = "chests." + block.getX() + "&" + block.getY() + "&" + block.getZ();
-                FileConfiguration config = plugin.getConfig();
-                if (config.getConfigurationSection(path) == null) {
-                    config.set(path + ".world", block.getWorld().getName());
-                    config.set(path + ".x", block.getX());
-                    config.set(path + ".y", block.getX());
-                    config.set(path + ".z", block.getX());
-                    player.sendMessage(colorize("&l&6Вы установили мусорный бак!"));
-                    garbagePlayers.remove(player);
+        FileConfiguration config = plugin.getConfig();
 
-                    plugin.saveConfig();
-                }
-            }
-        }
+        if (event.getAction() != Action.LEFT_CLICK_BLOCK) return;
+        Block block = event.getClickedBlock();
+        String path = String.format("chest.%d&%d&%d", block.getX(), block.getY(), block.getZ());
+
+        if (block.getType() != Material.CHEST || !garbagePlayers.contains(player)) return;
+        else if (Optional.ofNullable(config.getConfigurationSection(path)).isPresent()) return;
+
+        config.set(path + ".world", block.getWorld().getName());
+        config.set(path + ".x", block.getX());
+        config.set(path + ".y", block.getX());
+        config.set(path + ".z", block.getX());
+        player.sendMessage(colorize("&l&6Вы установили мусорный бак!"));
+        garbagePlayers.remove(player);
+
+        plugin.saveConfig();
     }
 }

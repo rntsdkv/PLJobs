309c010e2089ac7e377fbb90e05124bc5d504373
Renat <renat.sadykovrenat@yandex.ru>
2020-07-05 14:21:12 +0300

    Добавили эвент ломания блока у шахтера

--- src/main/java/ru/prisonlife/pljobs/events/MinerListener.java
+++ src/main/java/ru/prisonlife/pljobs/events/MinerListener.java
@@ -1,7 +1,20 @@
 package ru.prisonlife.pljobs.events;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import ru.prisonlife.Job;
+import ru.prisonlife.PrisonLife;
+import ru.prisonlife.Prisoner;
 import ru.prisonlife.plugin.PLPlugin;
+
+import static ru.prisonlife.pljobs.Main.minerBlockValues;
+import static ru.prisonlife.pljobs.Main.playersSalary;
 
 /**
  * @author rntsdkv
@@ -13,5 +26,23 @@ public class MinerListener implements Listener {
     private final PLPlugin plugin;
     public MinerListener(PLPlugin plugin) {
         this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        Prisoner prisoner = PrisonLife.getPrisoner(player);
+
+        Block block = event.getBlock();
+        String blockType = block.getType().name();
+
+        if (!minerBlockValues.containsKey(blockType)) return;
+
+        if (prisoner.getJob() != Job.MINER) return;
+
+        int price = minerBlockValues.get(blockType);
+
+        playersSalary.put(player, playersSalary.get(player) + price);
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.GREEN + String.format("+%d$", price)));
     }
 }
